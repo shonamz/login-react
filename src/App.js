1f30aa18f470d603146da601edfc0552cf0a28e2
Shokoufeh n <amitazh@aol.com>
2020-04-22 17:18:13 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    logins: [],
+    login: {
+      uname: "sample username",
+      password: "sample password"
+    }
+  };
+
+  componentDidMount() {
+    this.getLogin();
+  }
+
+  getLogin = _ => {
+    fetch("http://localhost:4000/logins")
+      .then(response => response.json())
+      .then(response => this.setState({ logins: response.data }))
+      .catch(err => console.error(err));
+  };
+  addLogin = _ => {
+    const { logins, login } = this.state;
+    fetch(
+      `http://localhost:4000/logins/add?uname=${login.uname}&password=${login.password}`
+    )
+      .then(this.getLogin)
+      .catch(err => console.err(err));
+  };
+  renderLogin = ({ id, uname }) => <div key={id}>{uname}</div>;
+
+  render() {
+    const { logins, login } = this.state;
+    return (
+      <div className="App">
+        {logins.map(this.renderLogin)}
+        <div>
+          <input
+            value={login.uname}
+            onChange={e =>
+              this.setState({ login: { ...login, uname: e.target.value } })
+            }
+          />
+          <input
+            value={login.password}
+            onChange={e =>
+              this.setState({ login: { ...login, password: e.target.value } })
+            }
+          />
+          <button onClick={this.addLogin}>Add login</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
